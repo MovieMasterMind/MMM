2b35804a21a33e74eb5bd639f25fa3f9acdd4de8
MichaelReinheimer <98563131+MichaelReinheimer@users.noreply.github.com>
2024-03-06 15:00:56 -0800

    Michael (#14)
    * Develop (#13)
    
    * updated readme using android studio
    
    * Updated README.md
    
    * Updated README.md
    
    * Updated README.md
    
    * Updated README.md
    
    * Would liking of merging dis one with de develop branch (#12)
    
    * securing da beg
    
    * securing da beg
    
    * securing da beg
    
    * Delete .idea/misc.xml
    
    * Delete .idea/gradle.xml
    
    ---------
    
    Co-authored-by: MichaelReinheimer <98563131+MichaelReinheimer@users.noreply.github.com>
    Co-authored-by: Antonio Flores <112218027+Antonio-Flores986@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: antonio.flores986 <antonio.flores986@myci.csuci.edu>
    Co-authored-by: ZenithPvP <ezkeed125@gmail.com>
    Co-authored-by: Antonio Flores <112218027+Antonio-Flores986@users.noreply.github.com>
    
    * Added API Calling code
    
    * API Called added and printed to home screen
    
    ---------
    
    Co-authored-by: antonio.flores986 <antonio.flores986@myci.csuci.edu>
    Co-authored-by: ZenithPvP <ezkeed125@gmail.com>
    Co-authored-by: Antonio Flores <112218027+Antonio-Flores986@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.github.kittinunf.fuel:fuel-android:2.3.1")
+    implementation ("com.github.kittinunf.fuel:fuel-gson:2.3.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    implementation("com.android.volley:volley:1.2.1")
+
+    implementation("com.google.code.gson:gson:2.8.9")
+
 }
--- app/src/main/java/com/example/mmm/MainActivity.kt
+++ app/src/main/java/com/example/mmm/MainActivity.kt
@@ -11,9 +11,24 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.RequestQueue
 import com.example.mmm.databinding.ActivityMainBinding
 
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import android.util.Log
+
 class MainActivity : AppCompatActivity() {
+
+    //Adding variables API
+    private var mRequestQueue: RequestQueue? = null
+//    private var mStringRequest: StringRequest? = null
+//    private val movieUrl = "//https://www.omdbapi.com/?t=batman&apikey=8081b028"
+//    private val searchUrl = "https://www.omdbapi.com/?t=batman&apikey=8081b028"
+
+
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -41,8 +56,124 @@ class MainActivity : AppCompatActivity() {
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setupWithNavController(navController) // Injecting the code for network request
+
+//        var apiRequestQueue: RequestQueue? = null
+
+        //Calling getData will get the API data from OMDB using the API, to get the JSON file
+        getData()
+
+
+
+
+
     }
+
+    //This works
+    private fun getData() {
+        // RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this)
+
+        // First URL for the movie details
+        val movieUrl = "https://www.omdbapi.com/?t=batman&apikey=8081b028"
+
+        // Second URL for the search results
+        val searchUrl = "https://www.omdbapi.com/?s=batman&apikey=8081b028"
+
+        // String Request for movie details
+        val movieRequest = StringRequest(Request.Method.GET, movieUrl,
+            { response ->
+                Log.d("MovieResponse", response)
+                val movie = Gson().fromJson(response, Movie::class.java)
+                displayMovieDetails(movie, null)
+            },
+            { error ->
+                Log.i("TAG", "Error fetching movie details: $error")
+            }
+        )
+
+        // String Request for search results
+        val searchRequest = StringRequest(Request.Method.GET, searchUrl,
+            { response ->
+                Log.d("SearchResponse", response)
+                val searchResult = Gson().fromJson(response, SearchResult::class.java)
+                displayMovieDetails(null, searchResult)
+            },
+            { error ->
+                Log.i("TAG", "Error fetching search results: $error")
+            }
+        )
+
+        // Add both requests to the request queue
+        mRequestQueue!!.add(movieRequest)
+        mRequestQueue!!.add(searchRequest)
+    }
+
+
+    private fun displayMovieDetails(movie: Movie?, searchResult: SearchResult?) {
+        val details = StringBuilder()
+
+        // Append details for the single movie
+        if (movie != null) {
+            details.append("Single Movie Details:\n")
+            details.append("Title: ${movie.title}\n")
+            details.append("Year: ${movie.year}\n")
+            details.append("Rated: ${movie.rated}\n")
+            details.append("Released: ${movie.released}\n")
+            details.append("Runtime: ${movie.runtime}\n")
+            details.append("Genre: ${movie.genre}\n")
+            details.append("Director: ${movie.director}\n")
+            details.append("Writer: ${movie.writer}\n")
+            details.append("Actors: ${movie.actors}\n")
+            details.append("Plot: ${movie.plot}\n")
+            details.append("Language: ${movie.language}\n")
+            details.append("Country: ${movie.country}\n")
+            details.append("Awards: ${movie.awards}\n")
+            details.append("Poster: ${movie.poster}\n")
+            details.append("Metascore: ${movie.metascore}\n")
+            details.append("imdbRating: ${movie.imdbRating}\n")
+            details.append("imdbVotes: ${movie.imdbVotes}\n")
+            details.append("imdbID: ${movie.imdbID}\n")
+            details.append("Type: ${movie.type}\n")
+            details.append("DVD: ${movie.dvd}\n")
+            details.append("BoxOffice: ${movie.boxOffice}\n")
+            details.append("Production: ${movie.production}\n")
+            details.append("Website: ${movie.website}\n")
+            details.append("Response: ${movie.response}\n")
+            details.append("\n")
+        }
+
+        // Append details for the search results
+        if (searchResult?.search != null) {
+            details.append("Search Results:\n")
+            for (item in searchResult.search) {
+                details.append("Title: ${item.title}\n")
+                details.append("Year: ${item.year}\n")
+                details.append("imdbID: ${item.imdbID}\n")
+                details.append("Type: ${item.type}\n")
+                details.append("Poster: ${item.poster}\n")
+                details.append("\n")
+            }
+        }
+
+        // Set the text of the TextView to the combined details
+        binding.movieDetailsTextView.text = details.toString()
+
+        // Scroll to the top of the ScrollView
+        binding.movieDetailsTextView.post { binding.movieDetailsTextView.scrollTo(0, 0) }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -54,4 +185,50 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+
+    data class Movie(
+        val title: String,
+        val year: String,
+        val rated: String,
+        val released: String,
+        val runtime: String,
+        val genre: String,
+        val director: String,
+        val writer: String,
+        val actors: String,
+        val plot: String,
+        val language: String,
+        val country: String,
+        val awards: String,
+        val poster: String,
+        val ratings: List<Rating>,
+        val metascore: String,
+        val imdbRating: String,
+        val imdbVotes: String,
+        val imdbID: String,
+        val type: String,
+        val dvd: String,
+        val boxOffice: String,
+        val production: String,
+        val website: String,
+        val response: String
+    )
+
+    data class Rating(
+        val source: String,
+        val value: String
+    )
+
+    data class SearchResult(
+        val search: List<MovieItem>
+    )
+
+    data class MovieItem(
+        val title: String,
+        val year: String,
+        val imdbID: String,
+        val type: String,
+        val poster: String
+    )
 }
