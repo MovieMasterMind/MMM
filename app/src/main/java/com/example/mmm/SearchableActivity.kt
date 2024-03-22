95faa7cf6d01a1dbdd603cecba6165fadf97b621
antonio <antonio.flores986@myci.csuci.edu>
2024-03-22 14:44:31 -0700

    changed search_results.xml gravity attribute from "left" to "start"

--- app/src/main/java/com/example/mmm/SearchableActivity.kt
+++ app/src/main/java/com/example/mmm/SearchableActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.Menu
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
