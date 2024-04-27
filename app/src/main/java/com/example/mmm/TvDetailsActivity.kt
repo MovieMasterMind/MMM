677943e447280e1803719f891d8d00f9951311f2
Eric <eric.hurtado627@myci.csuci.edu>
2024-04-26 22:51:17 -0700

    added streaming location

--- app/src/main/java/com/example/mmm/TvDetailsActivity.kt
+++ app/src/main/java/com/example/mmm/TvDetailsActivity.kt
@@ -1,8 +1,14 @@
 package com.example.mmm
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -39,19 +45,46 @@ class TvDetailsActivity : AppCompatActivity() {
         val url =
             "https://api.themoviedb.org/3/tv/$tvId?api_key=1f443a53a6aabe4de284f9c46a17f64c&language=en-US"
 
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
-            { response ->
-                displayTVDetails(response)
-            },
-            { error ->
-                Log.e("TVDetailsActivity", "Error fetching tv details: $error")
+        apiCallerForTV.getTVStreamingLocationJSON(tvId) { streamingDetails ->
+            Log.e("StreamingDetails", streamingDetails.toString())
+
+            //streamingDetails hold links of locations of the streaming service
+            val streamingDetailsFound = streamingDetails.isNotEmpty()
+            val textViewText = if (streamingDetailsFound) {
+                // If details are found, hide the TextView and set text to empty
+                runOnUiThread {
+                    findViewById<TextView>(R.id.titleBeforeStreamingServices).apply {
+                        text = ""
+                        visibility = View.GONE  // Hide the TextView
+                    }
+                }
+            } else {
+                // If no details are found, show the TextView and set the no-data message
+                runOnUiThread {
+                    findViewById<TextView>(R.id.titleBeforeStreamingServices).apply {
+                        text = "No streaming platforms found :("
+                        visibility = View.VISIBLE  // Show the TextView
+                    }
+                }
             }
-        )
 
-        Volley.newRequestQueue(this).add(jsonObjectRequest)
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                { response ->
+                    // Existing code to display movie details
+                    displayTVDetails(response, streamingDetails)
+                },
+                { error ->
+                    Log.e("MovieDetailsActivity", "Error fetching movie details: $error")
+                }
+            )
+
+            Volley.newRequestQueue(this).add(jsonObjectRequest)
+        }
+        apiCallerForTV.getTVStreamingLocationJSON(1396) { streamingDetails ->
+            Log.e("StreamingDetailsBBBBBBBB", streamingDetails.toString())
+        }
     }
-
-    private fun displayTVDetails(tvDetails: JSONObject) {
+    private fun displayTVDetails(tvDetails: JSONObject, streamingDetails: Map<String, String>) {
         tvDetailsObj = tvDetails
         val titleTextView: TextView = findViewById(R.id.TvTitle)
         val overviewTextView: TextView = findViewById(R.id.TvOverview)
@@ -87,6 +120,58 @@ class TvDetailsActivity : AppCompatActivity() {
             Glide.with(this).load(posterUrl).into(posterImageView)
         }
 
+        Log.e("WHY", streamingDetails.toString())
+
+
+        val streamingLayout: LinearLayout = findViewById(R.id.streamingLayout)
+
+
+        // Create buttons for streaming services
+        for ((service, link) in streamingDetails) {
+            val button = Button(this)
+            button.text = service
+            val colors = getColorForService(service)
+            colors?.let { (backgroundColor, textColor) ->
+                button.setBackgroundColor(backgroundColor)
+                button.setTextColor(textColor)
+            }
+            button.setOnClickListener {
+                // Open the link in the browser when the button is clicked
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(link)
+                startActivity(intent)
+            }
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.setMargins(
+                0,
+                0,
+                0,
+                24
+            ) // Add bottom margin to create spacing between buttons
+            button.layoutParams = params
+            streamingLayout.addView(button)
+        }
+    }
+
+    private val serviceColorsMap = mapOf(
+        "netflix" to Pair(Color.BLACK, Color.parseColor("#E50914")),
+        "prime" to Pair(Color.parseColor("#00A8E1"), Color.BLACK),
+        "hulu" to Pair(Color.parseColor("#1CE783"), Color.BLACK),
+        "apple" to Pair(Color.GRAY, Color.BLACK),
+        "peacock" to Pair(Color.BLACK, Color.WHITE),
+        "hbo" to Pair(Color.WHITE, Color.BLACK),
+        "disney" to Pair(Color.parseColor("#000137"), Color.parseColor("#FFFFFF")),
+        "paramount" to Pair(Color.parseColor("#0164FF"), Color.parseColor("#FFFFFF"))
+
+
+        // Add more services and colors as needed - "service" to Pair(Color.color, Color.color), -
+    )
+
+    private fun getColorForService(service: String): Pair<Int, Int>? {
+        return serviceColorsMap[service.lowercase(Locale.ROOT)]
     }
 
     override fun onSupportNavigateUp(): Boolean {
